a46f607d2723ed2f8bb875badbee01ec5c02b1ec
jelenailic7 <ilic.jelena20@gmail.com>
2018-02-11 19:58:17 +0100

    II nacin

--- src/shared/home.service.ts
+++ src/shared/home.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/observable/from';
+//import 'rxjs/add/observable/from';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 
 @Injectable()
 export class HomeService {
 
     _url = 'https://api.github.com/users/';
-     posts;
+ 
     constructor(public http: HttpClient ){
     
     }
+
     getRepos(username) {
         const url = this._url + username + '/repos';
-        return Observable.from(this.http.get(url));         
+        // return Observable.from(this.http.get(url)); 
+        return new Observable((o: Observer <any>) => {
+            this.http.get(url).subscribe((data:any[])=>{
+                o.next(data);
+                o.complete();
+            });
+        });
     }
 }
-
-
+//II NACIN
